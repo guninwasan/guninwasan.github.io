58c3981063cced5cd25881aeb60ad1bcf81c779e
Gunin Wasan <98042082+guninwasan@users.noreply.github.com>
2024-09-17 13:28:01 -0400

    Update personal info (#4)
    Co-authored-by: Gunin Wasan <guninw1001@gmail.com>

---
+++ experience.js
@@ -0,0 +1,99 @@
+const ZenList = [
+  `Developed features for a web application using React, Redux, and
+  TypeScript allowing users to create and manage their insurance
+  policies.`,
+  `
+    Worked closely with UI/UX designers to translate wireframes and
+    mockups into functional code, ensuring a visually appealing and
+    intuitive user interface.
+  `,
+  `
+      Assisted in the development and implementation of a continuous integration and deployment (CI/CD) pipeline, automating the build, testing, and deployment processes, resulting in faster release cycles and improved software quality.
+      `,
+];
+
+const CFCSList = [
+  `Implemented features and modules for React Native and TypeScript
+    applications that boosted the team's progress and helped reach the
+    client's deadlines.`,
+  `
+    Reduced errors in the code by approximately 66% through
+    participating in code reviews, debugging, and app testing.
+      `,
+  `
+    Collaborated with cross-functional teams in an agile environment to
+              build a mobile application for a client which is to be used in 8+
+              countries.
+      `,
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+  const experiences = [
+    {
+      company: "Zensurance",
+      logo: "/assets/img/zen-logo.png",
+      role: "Software Developer Co-op",
+      description: ZenList,
+      tools:
+        "MongoDB, Salesforce, TypeScript, React, Redux, Sentry, Docker, Amplitude",
+      period: "May 2023 - Sept. 2024",
+      location: "Toronto, ON",
+      link: "https://www.zensurance.com/",
+    },
+    {
+      company: "CFCS Cloud Solutions",
+      logo: "/assets/img/cfcs_logo.jpeg",
+      role: "Software Developer Intern",
+      description: CFCSList,
+      tools: "React Native, Figma, TypeScript, Redux, Jest",
+      period: "May 2022 - Aug. 2022",
+      location: "Delhi, India",
+      link: "https://www.cfcs.co.in",
+    },
+  ];
+
+  const experienceContainer = document.getElementById("experience-container");
+
+  experiences.sort(
+    (a, b) =>
+      new Date(b.period.split(" - ")[1]) - new Date(a.period.split(" - ")[1])
+  );
+
+  experiences.forEach((exp) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+            <div class="card-content">
+                <div class="row">
+                    <div class="col s12 m2">
+                        <a href="${exp.link}" target="_blank"><img alt="${
+      exp.company
+    } logo" src="${exp.logo}" class="responsive-img center-block" /></a>
+                    </div>
+                    <div class="col s12 m10">
+                        <p>
+                            <span class="card-title"><a href="${
+                              exp.link
+                            }" target="_blank" class="teal-text hoverline">${
+      exp.company
+    }</a></span>
+                        </p>
+                    </div>
+                </div>
+                <div class="role brown-text text-darken-2">${exp.role}</div>
+                <ul>
+                    ${exp.description
+                      .map((item) => `<li>${item}</li>`)
+                      .join("")}
+                    <li><b>Tools:</b> ${exp.tools}</li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <span>${exp.period} | ${exp.location}</span>
+            </div>
+        `;
+
+    experienceContainer.appendChild(card);
+  });
+});
---
+++ skills.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const skills = {
+    languages: [
+      { name: "C++", img: "/assets/img/c++.png" },
+      { name: "JavaScript", img: "/assets/img/js.png" },
+      { name: "Python", img: "/assets/img/python.png" },
+      { name: "C", img: "/assets/img/c.png" },
+    ],
+    webTechnologies: [
+      { name: "HTML5", img: "/assets/img/html-dark.png" },
+      { name: "CSS3", img: "/assets/img/css-dark.png" },
+      { name: "React", img: "/assets/img/react.png" },
+      { name: "Next.js", img: "/assets/img/next-light.png" },
+      { name: "TypeScript", img: "/assets/img/ts.png" },
+      { name: "Django", img: "/assets/img/django-logo.webp" },
+      { name: "Flask", img: "/assets/img/flask-logo.png" },
+    ],
+    databasesAndLibraries: [
+      { name: "PostgreSQL", img: "/assets/img/postgresql-logo.png" },
+      { name: "MySQL", img: "/assets/img/mysql-logo-1-300x300.jpg" },
+      { name: "MongoDB", img: "/assets/img/mongo.png" },
+      { name: "NumPy", img: "/assets/img/numpy-logo-1-500x500.jpg" },
+      { name: "Pandas", img: "/assets/img/pandas-logo-2-500x500.jpg" },
+      { name: "OpenCV", img: "/assets/img/opencv-logo-1-500x500.jpg" },
+      { name: "scikit-learn", img: "/assets/img/sk-learn-logo-1-500x500.jpg" },
+      { name: "matplotlib", img: "/assets/img/matplotlib-logo-1-500x500.jpg" },
+    ],
+  };
+
+  const skillsContainer = document.getElementById("skills-container");
+
+  const createSkillCard = (title, skills) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardContent = document.createElement("div");
+    cardContent.className = "card-content";
+
+    const cardTitle = document.createElement("h4");
+    cardTitle.className = "brown-text light";
+    cardTitle.textContent = title;
+
+    const row = document.createElement("div");
+    row.className = "row text-center";
+
+    skills.forEach((skill) => {
+      const col = document.createElement("div");
+      col.className = "col s4 m2";
+
+      const img = document.createElement("img");
+      img.alt = skill.name;
+      img.src = skill.img;
+      img.className = "responsive-img";
+
+      col.appendChild(img);
+      col.appendChild(document.createTextNode(skill.name));
+      row.appendChild(col);
+    });
+
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(row);
+    card.appendChild(cardContent);
+
+    return card;
+  };
+
+  skillsContainer.appendChild(createSkillCard("Languages", skills.languages));
+  skillsContainer.appendChild(
+    createSkillCard("Web Technologies", skills.webTechnologies)
+  );
+  skillsContainer.appendChild(
+    createSkillCard("Databases and Libraries", skills.databasesAndLibraries)
+  );
+});
